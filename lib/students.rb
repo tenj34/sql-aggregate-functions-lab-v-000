928ff449689b694f304e8c1b864fdfe248055ff1
Juan Tenezaca <tenj34@gmail.com>
2018-09-15 21:21:04 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,6 +1,16 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
+=begin
+CREATE TABLE students
+    (id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER,
+    gpa FLOAT,
+    tardies INTEGER);
+=end
+
 
 def highest_student_gpa
+  @db.execute("SELECT MIN(gpa) FROM students;")
 end
 
 def lowest_student_gpa
